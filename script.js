d19034b2c3e4eb0ed01bbf2e64bcca85c7e4fa71
tya3210 <tarogamorashita@gmail.com>
2025-01-28 09:04:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 // 地図の初期化
-const map = L.map('map').setView([35.6895, 139.6917], 10);
+const map = L.map('map').setView([36.5, 138.5], 5);
 
 // 地理院タイルのレイヤーを定義
 const standardMap = L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
     attribution: '国土地理院',
     maxZoom: 18,
+    minZoom: 5,
 });
 
 const paleMap = L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
     attribution: '国土地理院',
     maxZoom: 18,
+    minZoom: 5,
 });
 
 const aerialPhoto = L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg', {
     attribution: '国土地理院',
     maxZoom: 18,
+    minZoom: 5,
 });
 
 // レイヤーを追加
